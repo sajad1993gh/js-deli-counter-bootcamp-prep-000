7456ebe42b4860f786ebea6ebac381d9cf0a57ab
Sajad Gholamzadehrizi <sajad1993gh@gmail.com>
2019-01-15 00:50:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function currentLine(katzDeliLine){
     for(let i=0 ; i< katzDeliLine.length ; i++){
     lineString = lineString + (i+1) + ". " + katzDeliLine[i] +", "
            }
+    lineString = lineString - ","
    return lineString
        }
 }
